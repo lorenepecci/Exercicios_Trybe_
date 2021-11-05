6cfefce82ac07ee2df71dc83897967b376524299
Lorene Pecci <lorenepecci@gmail.com>
2021-11-05 16:43:33 -0300

    ex1 feito

---
+++ 1-Fundamentos/4.1JavaScript-primeiros-passos/exercicio1/script.js
@@ -0,0 +1,9 @@
+let num1= 30;
+let num2=50;
+
+console.log("A multiplicação fica:", num1*num2);
+console.log("A divisão fica:", num1/num2);
+console.log("A soma fica:", num1+num2);
+console.log("A dimminuição fica:", num1-num2);
+console.log("Um elevado ao outro fica:", num1**num2);
+
