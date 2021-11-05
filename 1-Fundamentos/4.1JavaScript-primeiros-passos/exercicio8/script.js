fa6e27ac72e36c5b5a576d60c34d95095137fdc4
Lorene Pecci <lorenepecci@gmail.com>
2021-11-05 18:06:29 -0300

    ex8 feito

---
+++ 1-Fundamentos/4.1JavaScript-primeiros-passos/exercicio8/script.js
@@ -0,0 +1,13 @@
+/*Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+Bonus: use somente um if .*/
+
+const n1=3;
+const n2=7;
+const n3=5;
+
+if (n1%2==0 || n2%2==0 || n3%2==0){
+    console.log("True")
+}else{
+    console.log("false")
+}
+
