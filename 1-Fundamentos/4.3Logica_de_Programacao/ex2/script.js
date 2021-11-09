9ac14a81e958ce97360f236221561ca0d7e4c56b
Lorene Pecci <lorenepecci@gmail.com>
2021-11-09 11:57:29 -0300

    ex2

---
+++ 1-Fundamentos/4.3Logica_de_Programacao/ex2/script.js
@@ -0,0 +1,11 @@
+/*2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.*/
+let n=5;
+let symbol = '*';
+let line = '';
+
+
+for (let i = 0; i < n; i += 1) {
+    line=line+symbol;
+    console.log(line)
+};
+
