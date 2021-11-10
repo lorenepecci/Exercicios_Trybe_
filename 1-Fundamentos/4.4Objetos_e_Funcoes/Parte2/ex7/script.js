2292cd5e2c00188b33942290182552e4162cb323
Lorene Pecci <lorenepecci@gmail.com>
2021-11-10 18:18:05 -0300

    rever ex7

---
+++ 1-Fundamentos/4.4Objetos_e_Funcoes/Parte2/ex7/script.js
@@ -0,0 +1,28 @@
+/* 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word . */
+let a='trybe';;
+let b='be';
+let palavraA=a.split('').reverse();
+let palavraB=b.split('').reverse();
+let retorno;
+let negativo=0;
+
+function comparacaoFinalPalavras(palavra1,palavra2){
+    
+    for(let i=(palavra2.length); i>0 ;i--){
+        if(palavra2[i]!==palavra1[i]){
+            negativo+=1;
+        }
+    }
+    if(negativo>0){
+        retorno=false;
+    }else{
+        retorno=true;
+    }
+    return retorno;
+}
+console.log(palavraB);
+console.log(palavraA);
+console.log(comparacaoFinalPalavras(palavraA,palavraB));
+
+
+
