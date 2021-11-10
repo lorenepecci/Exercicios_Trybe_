44c3067e8993fc6414172909fccf9e493793bd91
Lorene Pecci <lorenepecci@gmail.com>
2021-11-10 15:12:29 -0300

    parte1 feita

---
+++ 1-Fundamentos/4.4Objetos_e_Funcoes/Parte1/ex7.js
@@ -0,0 +1,27 @@
+/* 7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações: 
+{
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editor: 'Rocco',
+}*/ 
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+      {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editor: 'Rocco',
+      }
+    ],
+  }; 
+  
+  console.log("O livro favorito da", leitor.nome, leitor.sobrenome, "se chama", leitor.livrosFavoritos[1].titulo);
+  
