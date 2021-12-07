2f76f8c63b93744feede1c68984ac972e778edbb
Lorene Pecci <lorenepecci@gmail.com>
2021-12-07 12:58:50 -0300

    ex1f

--- 1-Fundamentos/7.2Objects_Metodos/ex1/script.js
+++ 1-Fundamentos/7.2Objects_Metodos/ex1/script.js
@@ -37,22 +37,29 @@ const order = {
   /* Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
 Note que o parâmetro da função já está sendo passado na chamada da função.
  */
-/*   const customerInfo = (order) => {
-    console.log(`Olá ${order.order['delivery']['deliveryPerson']} , entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address['street']}, Nº: ${order['address']['number']}, AP: ${order['address']['apartment']}`);
-  }
+ const customerInfo = (order) => {
+  const address = 'address';
+  const deliveryPerson = order.order.delivery.deliveryPerson;
+  const customerName = order['name'];
+  const customerPhone = order['phoneNumber'];
+  const street = order[address].street;
+  const number = order[address].number;
+  const apartment = order[address].apartment;
+
+  console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, R. ${street}, Nº: ${number}, AP: ${apartment}`);
+}
+customerInfo(order);
+
   
- customerInfo(order); */
-  
- /* Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+/* Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
 Modifique o nome da pessoa compradora.
 Modifique o valor total da compra para R$ 50,00. */
 
 const orderModifier = (order) => {
-    pizzaNames =Object.keys(order.order.pizza);
+  pizzaNames =Object.keys(order.order.pizza);
     drinksNames=Object.keys(order.order.drinks);
     order.name= 'Luiz Silva';
+    order.payment.total= 50;
    console.log(`Olá ${order.name}, o total do seu pedido de ${pizzaNames[0]}, ${pizzaNames[1]} e ${drinksNames[0]} é R$ ${order.payment.total}.`)
 }
-  
 orderModifier(order); 
-  
