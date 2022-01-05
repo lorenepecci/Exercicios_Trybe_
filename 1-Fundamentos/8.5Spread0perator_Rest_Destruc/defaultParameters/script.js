2bcde16ff4ec2a0ac186e29228f479f99b99514f
Lorene Pecci <lorenepecci@gmail.com>
2022-01-05 15:45:52 -0300

    g

---
+++ 1-Fundamentos/8.5Spread0perator_Rest_Destruc/defaultParameters/script.js
@@ -0,0 +1,9 @@
+/* Para Fixar
+Para praticar, escreva uma função multiply que multiplique dois números passados como argumentos. Atribua como default o valor 1 caso não seja passado nenhum valor como segundo parâmetro.
+Copiar */
+const multiply = (number , value = 1) => {
+  // Escreva aqui a sua função
+  return number*value;
+};
+
+console.log(multiply(8));
