b24a068f6d09979d079a509459603ad8e5e77d8d
Lorene Pecci <lorenepecci@gmail.com>
2022-01-05 16:24:52 -0300

    j

---
+++ 1-Fundamentos/8.5Spread0perator_Rest_Destruc/ex4/script.js
@@ -0,0 +1,49 @@
+/* 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+Dica: use object destructuring .
+Copiar */
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo 
+const filterPeople = (lista) => {
+    const apenasObj = lista.reduce((ac , pessoa) =>{
+        let nome = '';
+    const {name, bornIn, nationality} = pessoa;
+    if (nationality === 'Australian') {
+        if (bornIn >= 1900 && bornIn <= 2000) {
+            nome = name;
+        } 
+    }
+    return ac + ' ' + nome ;
+    }, '')
+
+       
+    return `pessoas australianas que nasceram no século 20: ${apenasObj}` 
+}
+
+console.log(filterPeople(people))
