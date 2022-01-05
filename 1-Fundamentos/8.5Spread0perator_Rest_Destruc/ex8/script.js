ea06e0d11cee0dcbce3a9e82541d5620d2540b1f
Lorene Pecci <lorenepecci@gmail.com>
2022-01-05 16:54:08 -0300

    h

--- 1-Fundamentos/8.5Spread0perator_Rest_Destruc/ex8/script.js
+++ 1-Fundamentos/8.5Spread0perator_Rest_Destruc/ex8/script.js
@@ -4,7 +4,12 @@ Copiar */
 // escreva greet abaixo
 const greet = (...args) => args.reduce((ac, a )=> a+' '+ac)
 
+const greet2 = (arg1 , arg2 ='Hi') => `${arg2} ${arg1}`
 
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
 console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
+
+console.log(greet2('John')) // 'Hi John'
+console.log(greet2('John', 'Good morning')) // 'Good morning John'
+console.log(greet2('Isabela', 'Oi')) // 'Oi Isabela'
