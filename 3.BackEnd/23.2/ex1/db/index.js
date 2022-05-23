602aaba4212f1fd1988ad019b00bb37aeb0c135a
Lorene Pecci <lorenepecci@gmail.com>
2022-05-23 12:24:55 -0300

    instalçao de dependencias, index e db configurados

---
+++ 3.BackEnd/23.2/ex1/db/index.js
@@ -0,0 +1,11 @@
+const mysql = require( 'mysql2/promise' );
+
+const connection = mysql.createPool( {
+  host: localhost,
+  port: 3306,
+  user: "lorene",
+  password: "lor55fdspp",
+  database: "cep_lookup"
+} )
+
+module.exports = { connection };
---
+++ 3.BackEnd/23.2/ex1/routes/index.js
@@ -0,0 +1,4 @@
+const express = require( 'express' );
+const routes = express.Router();
+//const cepController = 
+routes.use('/cep', cepController)
